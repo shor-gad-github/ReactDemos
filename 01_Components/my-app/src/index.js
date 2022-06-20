3af9ba9a4f09e3264357691b89931d3c20c3ec59
shor-gad-github <shorgad@outlook.com>
2022-06-20 18:13:33 +0300

    CountriesList Homework

--- 01_Components/my-app/src/index.js
+++ 01_Components/my-app/src/index.js
@@ -4,6 +4,36 @@ import ComponentAsClass from './ComponentAsClass.js';
 import OtherComponent from './MyOtherComponent.js';
 import './index.css';
 
+function GetCountries()
+{
+  let countriesObjArray = [{countryCode:234243,countryName:"France",Capital:"Paris"},
+                           {countryCode:567567,countryName:"Israel",Capital:"Jerusalem"},
+                           {countryCode:456768,countryName:"USA",Capital:"Washington"}];
+
+  return countriesObjArray;
+}
+
+
+//--Demo To Generate Component out of standart JS Array Object  
+function CountriesList()
+{
+  //--Array Object
+  const countries  = GetCountries();
+  return(
+    <ul> 
+    { //--Use { } to move from React JSX-HTML to JS code area 
+      countries.map((country) =>
+        <li>{`[${country.countryCode}]  -${country.countryName}(${country.Capital})`}</li> 
+      )
+    } 
+  </ul>);
+
+}
+
+
+
+
+
 //--Demo To Generate Component out of standart JS Array Object  
 function Countries()
 {
@@ -37,7 +67,7 @@ function MyCompositeComponent()
 //Create he Root Div For REACT
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <Countries />
+   <CountriesList />
   //  <ComponentAsClass />
   //  <MyCompositeComponent />
 );
