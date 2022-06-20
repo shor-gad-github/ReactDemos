a3f62eee4d95fce91f2f6fb459676518a2d493df
shor-gad-github <shorgad@outlook.com>
2022-06-20 18:46:47 +0300

    Composite WebSite

---
+++ 01_Components/my-app/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Footer() {
+    return (
+        <footer>
+            <p>&copy; You can copy, edit and publish this template but please leave a link to our website | <a href="https://html5-templates.com/" target="_blank" rel="nofollow">HTML5 Templates</a></p>
+            <address> Contact: <a href="mailto:me@example.com">Mail me</a>
+            </address>
+        </footer>
+    );
+}
