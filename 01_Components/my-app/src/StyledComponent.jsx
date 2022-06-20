8d8d7d26d189e10325b2de56e83852a107b0f4ce
shor-gad-github <shorgad@outlook.com>
2022-06-20 20:20:48 +0300

    Styling

--- 01_Components/my-app/src/StyledComponent.jsx
+++ 01_Components/my-app/src/StyledComponent.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
 
 function  StyledComponent() {
-   let myStyle =  {color:"blue",border:"3px solid blue"};
+
+    let myStyle =  {color:"blue",border:"3px solid blue"};
    //Edit Style as JSON OBJECT
+
+    let isAuthnticated = false; 
+
+    //Conditional Styling
+    let userStyle = {};
+    if (isAuthnticated)
+      userStyle = {color:"Green",border:"3px solid Green"};
+      else
+      userStyle = {color:"Red",border:"3px solid Red"};
 
     return ( 
        <>
@@ -11,6 +21,11 @@ function  StyledComponent() {
         <h1 style={{color:"blue",border:"3px solid blue"}}>HTML WITH STYLE 2</h1>
         <br></br>
         <h1 style={myStyle}>HTML WITH STYLE 3</h1>
+        <br></br>
+        <h1 style={{backgroundColor:"blue"}}>HTML WITH STYLE 3</h1>
+        <br></br>
+        <h1 style={userStyle}>HTML WITH STYLE 3</h1>
+
         </>
      );
 }
