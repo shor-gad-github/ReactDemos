d41607933bd138de34ce28dbedd362a8f9ce9202
shor-gad-github <shorgad@outlook.com>
2022-07-04 19:26:20 +0300

    FORM

--- Demos/demos-app/src/04_EventsAndForms/01_FormSubmit.jsx
+++ Demos/demos-app/src/04_EventsAndForms/01_FormSubmit.jsx
@@ -4,33 +4,38 @@ import './bootstrap.min.css'
 export default class Contuct extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { firstname: "", email: "" };
+        this.state = { name: "", email: "" };
 
+        this.nameChanged = this.nameChanged.bind(this);
+        this.emailChanged = this.emailChanged.bind(this);
+    }
+
+    nameChanged(e) {
+
+    }
+
+    emailChanged(e) {
 
     }
 
     render() {
         return (
-            <div className='jumbotron' style={{ width: "700px" }}>
-                <h1>Send Your Mail and we will contact you:</h1>
-                <form>
-                    <div className="form-group row">
-
-                        <div className="col-sm-10">
-                            <label for="name" className="col-sm-2 col-form-label">Email</label>
-                            <input type="text" className="form-control" id="name" />
+            <div className="container" style={{ width: "700px" }}>
+                <div className='jumbotron' >
+                    <h1>Send Your Email:</h1>
+                    <form>
+                        <div className="form-group row">
+                            <label for="name" className="col-sm-2 col-form-label">Name:</label>
+                            <input type="text" onChange={this.nameChanged} className="form-control" id="name" placeholder="name" />
                         </div>
-                    </div>
-                    <div className="form-group">
-                        <label for="inputEmail" className="form-label mt-4">Email address</label>
-                        <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
-                    </div>
-
-
-                </form>
+                        <div className="form-group row">
+                            <label for="inputEmail" className="form-label mt-4">Your Email:</label>
+                            <input type="email" onChange={this.emailChanged} className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
-
 
 }
