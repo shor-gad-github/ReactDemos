7c5f5e45ef654b4ac4c40c8a2ad9de1899d7553a
shor-gad-github <shorgad@outlook.com>
2022-07-04 19:49:33 +0300

    FINISH

--- Demos/demos-app/src/04_EventsAndForms/01_FormSubmit.jsx
+++ Demos/demos-app/src/04_EventsAndForms/01_FormSubmit.jsx
@@ -8,14 +8,27 @@ export default class Contuct extends React.Component {
 
         this.nameChanged = this.nameChanged.bind(this);
         this.emailChanged = this.emailChanged.bind(this);
+        this.submit = this.submit.bind(this);
     }
 
     nameChanged(e) {
-
+        let inputName = e.target.value;
+        this.setState({ name: inputName });
     }
 
     emailChanged(e) {
+        let inputEmail = e.target.value;
+        this.setState({ email: inputEmail });
+    }
 
+    submit(e) {
+        e.preventDefault();//DISABLE AUTO SUBMIT
+        let values = "name=" + this.state.name + "," + "email=" + this.state.email;
+        console.log(values);
+        alert(values);
+        //AJAX REQUEST ASP.NET API POST CONTRLLER
+
+        // this.state
     }
 
     render() {
@@ -23,7 +36,7 @@ export default class Contuct extends React.Component {
             <div className="container" style={{ width: "700px" }}>
                 <div className='jumbotron' >
                     <h1>Send Your Email:</h1>
-                    <form>
+                    <form onSubmit={this.submit}>
                         <div className="form-group row">
                             <label for="name" className="col-sm-2 col-form-label">Name:</label>
                             <input type="text" onChange={this.nameChanged} className="form-control" id="name" placeholder="name" />
@@ -32,7 +45,11 @@ export default class Contuct extends React.Component {
                             <label for="inputEmail" className="form-label mt-4">Your Email:</label>
                             <input type="email" onChange={this.emailChanged} className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
                         </div>
+                        <hr></hr>
+                        <input type="submit" value="send" />
                     </form>
+
+
                 </div>
             </div>
         );
