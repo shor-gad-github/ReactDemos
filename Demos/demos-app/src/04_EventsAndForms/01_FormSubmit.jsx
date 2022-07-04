4e7a610dd58db5a82e13ed74ed2554893d050e33
shor-gad-github <shorgad@outlook.com>
2022-07-04 20:06:21 +0300

    SUBMIT

--- Demos/demos-app/src/04_EventsAndForms/01_FormSubmit.jsx
+++ Demos/demos-app/src/04_EventsAndForms/01_FormSubmit.jsx
@@ -9,6 +9,16 @@ export default class Contuct extends React.Component {
         this.nameChanged = this.nameChanged.bind(this);
         this.emailChanged = this.emailChanged.bind(this);
         this.submit = this.submit.bind(this);
+        this.inputChanged = this.inputChanged.bind(this);
+    }
+
+    inputChanged(e) {
+        //--GENRIC UPDATE FOR ALL INPUTS EXSIT IN FORM--
+
+        let inputName = e.target.name;
+        let newValue = e.target.value;
+        this.setState({ [inputName]: newValue });
+
     }
 
     nameChanged(e) {
@@ -31,6 +41,9 @@ export default class Contuct extends React.Component {
         // this.state
     }
 
+
+
+
     render() {
         return (
             <div className="container" style={{ width: "700px" }}>
@@ -39,11 +52,11 @@ export default class Contuct extends React.Component {
                     <form onSubmit={this.submit}>
                         <div className="form-group row">
                             <label for="name" className="col-sm-2 col-form-label">Name:</label>
-                            <input type="text" onChange={this.nameChanged} className="form-control" id="name" placeholder="name" />
+                            <input type="text" onChange={this.inputChanged} className="form-control" name="name" id="name" placeholder="name" />
                         </div>
                         <div className="form-group row">
                             <label for="inputEmail" className="form-label mt-4">Your Email:</label>
-                            <input type="email" onChange={this.emailChanged} className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Enter email" />
+                            <input type="email" onChange={this.inputChanged} className="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email" />
                         </div>
                         <hr></hr>
                         <input type="submit" value="send" />
